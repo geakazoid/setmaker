c866e7c1a079831f9c205aff3acb171b453ffabf
lintile <aaron@lintile.com>
2014-05-17 12:17:58 -0500

    Adding the ability to constrain the questions by chapter.

--- query.js
+++ query.js
@@ -3,7 +3,6 @@ var async = require("async");
 
 var database_filename = "questions-matthew.db";
 
-var query_by_type = "select * from questions where type=?;";
 
 // Carefully taken from:
 // http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
@@ -28,8 +27,22 @@ function shuffle(array, callback) {
   callback(null, array);
 }
  
-function retrieve_question_info(type, callback) {
-    db.all(query_by_type, type, function db_callback(err, rows) {
+function retrieve_question_info(type, constraints, callback) {
+    var query_by_type = "select * from questions where type=?";
+    var array_of_args = [type];
+    if (constraints.chapters.length != 0)
+    {
+        query_by_type += "and chapter in (?"
+        array_of_args.push(constraints.chapters[constraints.chapters.length -1]);
+        for (var i = constraints.chapters.length - 2; i >= 0; i--) {
+            array_of_args.push(constraints.chapters[i]);
+            query_by_type += ",?";
+        };
+        query_by_type += ")";
+    }
+    query_by_type += ";";
+
+    db.all(query_by_type, array_of_args, function db_callback(err, rows) {
         //Callback for db all
         var info = [];
         if (err) throw err;
@@ -41,7 +54,7 @@ function retrieve_question_info(type, callback) {
     });
 }
 
-function choose_n_random_ids(list, n, callback)
+function choose_n_random_ids(list, n, constraints, callback)
 {
     if (list.length < n)
         throw "Not enough questions or type for count!";
@@ -55,26 +68,29 @@ function choose_n_random_ids(list, n, callback)
     }); //end shuffle
 }
 
-function select_questions (distribution, callback) {
+function select_questions (distribution, constraints, callback) {
     async.concat(distribution, function (each_type, callback) {
         if (each_type.count > 0) {
-            retrieve_question_info(each_type.type, function retrieve_callback(err, list) {
-                choose_n_random_ids(list, each_type.count, function choose_callback(err, chosenlist) {
+            retrieve_question_info(each_type.type, constraints, function retrieve_callback(err, list) {
+                choose_n_random_ids(list, each_type.count, constraints, function choose_callback(err, chosenlist) {
                     callback(null, chosenlist);
                 });
             });
         }
     }, function(err, idlist) {
-        shuffle(idlist, function(err, shuffled_list) {
-            callback(null, shuffled_list);
-        });
+        if (!constraints.shuffle)
+        {
+            callback(null, idlist);
+        } else {
+            shuffle(idlist, function(err, shuffled_list) {
+                callback(null, shuffled_list);
+            });
+        }
     });
 }
 
 var db = new sqlite3.Database(database_filename, sqlite3.OPEN_READONLY);
-//db.on('trace', function(text) {
-//    console.log(text);
-//});
+//db.on('trace', function(text) {console.log(text);});
 var setspec = [
       { type: "A", count: 4 },
       { type: "G", count: 11 },
@@ -84,10 +100,16 @@ var setspec = [
       { type: "V", count: 1 },
       { type: "X", count: 1 }];
 
-select_questions(setspec, function(err, shuffled_list) {
-    for (var i = shuffled_list.length - 1; i >= 0; i--) {
-        var q = shuffled_list[i];
-        console.log(q.type + " Q: " + q.question);
+var constraints =
+    {
+        shuffle: true,
+        chapters: [12, 13, 14]
+    }
+
+select_questions(setspec, constraints, function(err, questionlist) {
+    for (var i = 0; i < questionlist.length; i++) {
+        var q = questionlist[i];
+        console.log(q.type + " " + (i+1) + ": " + q.question);
         if (q.startverse == q.endverse) {
             console.log("  A: " + q.answer + " (" + q.book + " " 
                 + q.chapter + ":" + q.startverse + ")\n");            
