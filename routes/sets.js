f0aa57a72aacfae3df1829e1160629f63bc77925
Jeremy Driscoll <jdriscoll@realnets.com>
2014-06-01 15:03:00 -0500

    added start of the setmaker form. this needs a lot of work, but works at a very basic level.

--- routes/sets.js
+++ routes/sets.js
@@ -1,12 +1,81 @@
 var express = require('express');
 var router = express.Router();
 
-//load query module
+// load query module
 var query = require('../models/query');
 
 // GET sets listing
 router.get('/', function(req, res) {
 	res.render('index', { title: 'Sets' });
+});
+
+// GET form to create new set(s)
+router.get('/new', function(req, res) {
+	var question_types = [{label: "According To", type: "a", total: "216", count: "4"},
+	                      {label: "General",      type: "g", total: "904", count: "11"},
+	                      {label: "Situation",    type: "s", total: "129", count: "1"},
+	                      {label: "Quote",        type: "q", total: "94",  count: "1"},
+	                      {label: "Reference",    type: "r", total: "94",  count: "1"},
+	                      {label: "Verse",        type: "v", total: "94",  count: "1"},
+	                      {label: "Context",      type: "x", total: "69",  count: "1"}];
+	var book = "matthew";
+    var num_chapters = 28;
+
+	res.render('sets/new', { title: 'Create A Set',
+		                        qts: question_types,
+		                        book: book,
+		                        num_chapters: num_chapters });
+});
+
+// POST form values to create new set(s)
+router.post('/create', function(req, res) {
+	
+	// default values
+	var shuffle = true;
+	var chapters = [];
+	var num_a = 4;
+	var num_g = 11;
+	var num_q = 1;
+	var num_r = 1;
+	var num_s = 1;
+	var num_v = 1;
+	var num_x = 1;
+	
+	// check question counts
+	if (req.body.num_a != '') { num_a = req.body.num_a }
+	if (req.body.num_g != '') { num_g = req.body.num_g }
+	if (req.body.num_q != '') { num_q = req.body.num_q }
+	if (req.body.num_r != '') { num_r = req.body.num_r }
+	if (req.body.num_s != '') { num_s = req.body.num_s }
+	if (req.body.num_v != '') { num_v = req.body.num_v }
+	if (req.body.num_x != '') { num_x = req.body.num_x }
+	
+	// check question order
+	if (req.body.question_order === 'Sequential') { shuffle = false }
+	// check chapters
+	if (typeof req.body.matthew !== 'undefined') { chapters = req.body.matthew }
+	
+	// set specification for standard question set
+	var setspec = [
+	      { type: "A", count: num_a },
+	      { type: "G", count: num_g },
+	      { type: "Q", count: num_q },
+	      { type: "R", count: num_r },
+	      { type: "S", count: num_s },
+	      { type: "V", count: num_v },
+	      { type: "X", count: num_x }];
+	
+	var constraints =
+    {
+        shuffle: shuffle,
+        chapters: chapters
+    }
+	
+	var questions = ''
+	query.select_questions(setspec, constraints, function(err, questionlist) {
+	     // render question set to the browser
+	     res.render('sets/standard', { title: req.body.set_title, questions: questionlist });
+	});
 });
 
 // GET standard 20 question set
